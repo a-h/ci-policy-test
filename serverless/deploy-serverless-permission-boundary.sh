668876381fea6fc00918ac95fcbcc212883fa120
Adrian Hesketh <adrianhesketh@hushmail.com>
2021-01-12 21:36:48 +0000

    Updated permission boundary.

---
+++ serverless/deploy-serverless-permission-boundary.sh
@@ -0,0 +1,2 @@
+# Deploy the global permission boundary.
+aws cloudformation deploy --stack-name=serverless-permission-boundary --template-file=serverless-permission-boundary.yaml --capabilities=CAPABILITY_NAMED_IAM --region=eu-west-1
--- serverless/deploy.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation deploy --stack-name=serverless-ci --template-file=permission-boundary.yaml --capabilities=CAPABILITY_NAMED_IAM --region=eu-west-1
