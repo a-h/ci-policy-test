8e4b8a32543cfcdc1251f75d76a03154b538a5f6
Anthony Lock <anthony.lock@infinityworks.com>
2021-04-21 12:10:31 +0100

    adding Iam changes

--- serverless/deploy-serverless-permission-boundary.sh
+++ serverless/deploy-serverless-permission-boundary.sh
@@ -1,2 +1,7 @@
 # Deploy the global permission boundary.
-aws cloudformation deploy --stack-name=serverless-permission-boundary --template-file=serverless-permission-boundary.yaml --capabilities=CAPABILITY_NAMED_IAM --region=eu-west-1
+aws cloudformation deploy \
+--stack-name=serverless-permission-boundary \
+--template-file=serverless-permission-boundary.yaml  \
+--capabilities=CAPABILITY_NAMED_IAM  \
+--region=eu-west-1 \
+--parameter-overrides Squad=MySquad
